93fcecda56906e4fa18fb54e43d6134d69891bee
Gabriel Rivas <saurer19@gmail.com>
2017-09-12 19:12:19 -0400

    admin index

--- src/resources/views/admin_dashboard.blade.php
+++ src/resources/views/admin_dashboard.blade.php
@@ -1,36 +1,5 @@
-<head>
-<title>RADIO SABOR LATINO 93.5 FM</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<link rel="stylesheet" type="text/css" href="{{asset('/css/admin.css')}}">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
-
-</head>
-    <body class="home">
-        <div class="container-fluid display-table">
-            <div class="row display-table-row">
-                <div class="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box" id="navigation">
-                    <div class="logo">
-                        <a hef="home.html">     
-                            <img src="{{asset('/images/logo3.png')}}"  >
-                        </a>
-                    </div>
-                    <div class="navi">
-                        <ul>
-                            <li class="active"><a href="#"><i class="fa fa-home" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Home</span></a></li>
-                            <li><a href="#"><i class="fa fa-tasks" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Shows</span></a></li>
-                            <li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Tianguis</span></a></li>
-                            <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Events</span></a></li>
-                            <li><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Users</span></a></li>
-                        </ul>
-                    </div>
-                </div>
+@extends('layouts.adminheader')
+@section('content')
                 <div class="col-md-10 col-sm-11 display-table-cell v-align">
                     <!--<button type="button" class="slide-toggle">Slide Toggle</button> -->
                     <div class="row">
@@ -48,31 +17,7 @@
                                 </nav>
             
                             </div>
-                            <div class="col-md-5">
-                                <div class="header-rightside">
-                                    <ul class="list-inline header-top pull-right">
-                                        
-                                        <li class="dropdown">
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                               <i class="fa fa-user" aria-hidden="true"></i><span class="hidden-xs hidden-sm">User</span></a>
-                                                <b class="caret"></b></a>
-                                            <ul class="dropdown-menu">
-                                                <li>
-                                                    <div class="navbar-content">
-                                                        <span>JS Krishna</span>
-                                                        <p class="text-muted small">
-                                                            me@jskrishna.com
-                                                        </p>
-                                                        <div class="divider">
-                                                        </div>
-                                                        <a href="#" class="view btn-sm active">View Profile</a>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+  
                         </header>
                     </div>
                     <div class="user-dashboard">
@@ -160,3 +105,4 @@
         <script type="text/javascript" src="{{asset('js/admin.js')}}"></script>
 
     </body>
+@endsection
---
+++ src/resources/views/layouts/adminheader.blade.php
@@ -0,0 +1,35 @@
+<head>
+<title>RADIO SABOR LATINO 93.5 FM</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="{{asset('/css/admin.css')}}">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
+</head>
+    <body class="home">
+        <div class="container-fluid display-table">
+            <div class="row display-table-row">
+                <div class="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box" id="navigation">
+                    <div class="logo">
+                        <a hef="home.html">     
+                            <img src="{{asset('/images/logo3.png')}}"  >
+                        </a>
+                    </div>
+                    <div class="navi">
+                        <ul>
+                            <li ><a href="{{ url('/admin/authenticate') }}"><i class="fa fa-home" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Home</span></a></li>
+                            <li><a href="{{ url('/api/shows') }}"><i class="fa fa-tasks" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Shows</span></a></li>
+                            <li><a href="{{ url('/api/sales') }}"><i class="fa fa-bar-chart" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Tianguis</span></a></li>
+                            <li><a href="{{ url('/api/events') }}"><i class="fa fa-user" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Events</span></a></li>
+                            <li><a href="{{ url('/api/sponsors') }}"><i class="fa fa-calendar" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Sponsors</span></a></li>
+                            <li><a href="{{ url('/api/volunteers') }}"><i class="fa fa-calendar" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Volunteers</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                @yield('content')
