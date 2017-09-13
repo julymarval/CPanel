9583be32940ca4a1a5a49b470d94232d45fcc502
Gabriel Rivas <saurer19@gmail.com>
2017-09-13 17:50:27 -0400

    template public

--- src/resources/views/about.blade.php
+++ src/resources/views/about.blade.php
@@ -1,44 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>RADIO SABOR LATINO 93.5 FM</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-  <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css')}}">
-
-</head>
-<body id="home" data-spy="scroll" data-target=".navbar" data-offset="60">
-
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="{{ url('/') }}">
-      <img src="{{asset('/images/logo3.png')}}"  >
-      </a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{ url('/') }}">RADIO</a></li>
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="{{ url('/events') }}">EVENTS</a></li>
-        <li><a href="{{ url('/tianguis') }}">TIANGUIS</a></li>
-        <li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RGJJ7RX543MDQ"target="_blank">DONA</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+@extends('layouts.publicheader')
+@section('content')
 
 
 <div id="about" class="container-fluid">
@@ -239,3 +200,4 @@
 
 </body>
 </html>
+@endsection
--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,52 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>RADIO SABOR LATINO 93.5 FM</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css')}}">
-
-</head>
-<body id="home" data-spy="scroll" data-target=".navbar" data-offset="60">
-
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#home">
-        <img src="{{asset('/images/logo3.png')}}"  >
-      </a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">RADIO
-          <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-              <li><a href="#home">HOME</a></li>
-            <li><a href="#team">TEAM</a></li>
-            <li><a href="#shows">SHOWS</a></li>
-            <li><a href="#contact">CONTACT</a></li>
-          </ul>
-        </li>
-        <li><a href="{{ url('/about') }}">ABOUT</a></li>
-        <li><a href="{{route('events.index')}}">EVENTS</a></li>
-        <li><a href="{{route('sales.index')}}">TIANGUIS</a></li>
-        <li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RGJJ7RX543MDQ"target="_blank">DONA</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+@extends('layouts.publicheader')
+@section('content')
 
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
@@ -224,3 +177,4 @@ src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHyMG1u6cbwDVjX3nT974k73S
 
 </body>
 </html>
+@endsection
