325f63bc3790f08fd607956a8e11b9ade638aa4c
julymarval <july.marval@gmail.com>
2017-09-13 17:14:29 -0400

    nuevas vistas

---
+++ src/resources/views/events/admin_event.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.adminheader')
+@section('content')
+                <div class="col-md-10 col-sm-11 display-table-cell v-align">
+                    <!--<button type="button" class="slide-toggle">Slide Toggle</button> -->
+                    <div class="row">
+                        <header>
+                            <div class="col-md-7">
+                                <nav class="navbar-default pull-left">
+                                    <div class="navbar-header">
+                                        <button type="button" class="navbar-toggle collapsed" data-toggle="offcanvas" data-target="#side-menu" aria-expanded="false">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></spadmin/authenticate#an>
+                                        </button>
+                                    </div>
+                                </nav>
+            
+                            </div>
+  
+                        </header>
+                    </div>
+                    <div class="user-dashboard">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 col-xs-12 gutter">
+    
+                                <div class="sales">
+                                    <h2>Events</h2>
+    
+                                    <div class="row">
+        
+        <table class="table table-bordered table-striped">
+            <thead >
+                <tr class="bg-info ">
+                    <th></th>
+                    <th style="text-align: center">Name</th>>
+                </tr>
+            </thead>
+            
+            <tbody id="list-itens">
+                
+                @foreach ($events as $event)
+                    <tr>
+                        <td style="width:140px; text-align: center">
+                            <div class="btn btn-sm btn-default"><a href="{{route('events.show', $event -> id)}}"><i class="icon-trash glyphicon glyphicon-eye-open text-primary"></i></div>
+                            <div class="btn btn-sm btn-default"><a href="{{route('events.edit', $event -> id)}}"><i class="icon-trash glyphicon glyphicon-edit text-primary"></i></div>
+                            <div class="btn btn-sm btn-default"><a href="{{route('events.destroy', $event -> id)}}"><i class="icon-trash glyphicon glyphicon-trash text-danger"></i></div>
+                        </td>
+                        <td> {{$event["name"] }} </td>
+                    </tr>
+                @endforeach      
+                
+            </tbody>    
+            
+        </table>
+        
+        
+        
+    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+    
+    
+    
+        
+        <script type="text/javascript" src="{{asset('js/admin.js')}}"></script>
+
+    </body>
+@endsection
---
+++ src/resources/views/volunteers/admin_volunteer.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.adminheader')
+@section('content')
+                <div class="col-md-10 col-sm-11 display-table-cell v-align">
+                    <!--<button type="button" class="slide-toggle">Slide Toggle</button> -->
+                    <div class="row">
+                        <header>
+                            <div class="col-md-7">
+                                <nav class="navbar-default pull-left">
+                                    <div class="navbar-header">
+                                        <button type="button" class="navbar-toggle collapsed" data-toggle="offcanvas" data-target="#side-menu" aria-expanded="false">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></spadmin/authenticate#an>
+                                        </button>
+                                    </div>
+                                </nav>
+            
+                            </div>
+  
+                        </header>
+                    </div>
+                    <div class="user-dashboard">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 col-xs-12 gutter">
+    
+                                <div class="sales">
+                                    <h2>Volunteers</h2>
+    
+                                    <div class="row">
+        
+        <table class="table table-bordered table-striped">
+            <thead >
+                <tr class="bg-info ">
+                    <th></th>
+                    <th style="text-align: center">Name</th>
+                </tr>
+            </thead>
+            
+            <tbody id="list-itens">
+                
+                @foreach ($volunteers as $volunteer)
+                    <tr>
+                        <td style="width:140px; text-align: center">
+                            <div class="btn btn-sm btn-default"><a href="{{route('volunteers.show', $volunteer -> id)}}"><i class="icon-trash glyphicon glyphicon-eye-open text-primary"></i></div>
+                            <div class="btn btn-sm btn-default"><a href="{{route('volunteers.edit', $volunteer -> id)}}"><i class="icon-trash glyphicon glyphicon-edit text-primary"></i></div>
+                            <div class="btn btn-sm btn-default"><a href="{{route('volunteers.destroy', $volunteer -> id)}}"><i class="icon-trash glyphicon glyphicon-trash text-danger"></i></div>
+                        </td>
+                        <td> {{$volunteer["name"] }} </td>
+                    </tr>
+                @endforeach      
+                
+            </tbody>    
+            
+        </table>
+        
+        
+        
+    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+    
+    
+    
+        
+        <script type="text/javascript" src="{{asset('js/admin.js')}}"></script>
+
+    </body>
+
+@endsection
