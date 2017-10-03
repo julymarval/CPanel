7c90f15580dc3247e86d5f5a7e392520d1fb797e
Gabriel <saurer19@gmail.com>
2017-10-03 13:48:31 -0400

    bug sales

--- src/resources/views/sales/sale.blade.php
+++ src/resources/views/sales/sale.blade.php
@@ -1,41 +1,43 @@
 @extends('layouts.publicheader')
 @section('content')
 
-<div id="tianguis" class="container-fluid text-center bg-grey"> 
-    <br><h2>TIANGUIS</h2><br>
-    <h4>What we are selling</h4>
-  <div class="row">
-      <table class="table table-striped">
-          <thead >
-          <tr>
-              <th style="text-align: center">Name</th>
-              <th style="text-align: center">Price</th>
-              <th style="text-align: center">Description</th>
-          </tr>
-          </thead>
+    <div id="tianguis" class="container-fluid text-center bg-grey">
+        <br><h2>TIANGUIS</h2><br>
+        <h4>What we are selling</h4>
+        <div class="row">
+            <table class="table table-striped">
+                <thead >
+                <tr>
+                    <th style="text-align: center">Name</th>
+                    <th style="text-align: center">Price</th>
+                    <th style="text-align: center">Description</th>
+                </tr>
+                </thead>
 
-          <tbody>
-          @foreach ($sales as $sale)
-              <tr>
-                  </td>
-                  <td> {{$sale["name"] }} </td>
-                  <td> {{$sale["price"] }}$ </td>
-                  <td> {{$sale["description"] }} </td>
-              </tr>
-          @endforeach
-          </tbody>
-      </table>
-      {{$sales}}
-  </div>
-  </div>
+                <tbody>
+                @if(count($sales) > 0)
+                    @foreach ($sales as $sale)
+                        <tr>
+                            </td>
+                            <td> {{$sale["name"] }} </td>
+                            <td> {{$sale["price"] }}$ </td>
+                            <td> {{$sale["description"] }} </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+            {{$sales}}
+        </div>
+    </div>
 
 
-<footer class="container-fluid text-center">
-    @include('includes.publicfooter')
-  </footer>
-</div>
-  <script type="text/javascript" src="{{asset('js/magic.js')}}"></script>
-  
-  </body>
-  </html>
+    <footer class="container-fluid text-center">
+        @include('includes.publicfooter')
+    </footer>
+    </div>
+    <script type="text/javascript" src="{{asset('js/magic.js')}}"></script>
+
+    </body>
+    </html>
 @endsection
